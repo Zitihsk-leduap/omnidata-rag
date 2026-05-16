c2046b8c906861231bd3f80e6c4f989951d85336
Zitihsk-leduap <kshitizpaudel5@gmail.com>
2026-05-16 16:34:08 +0545

    model changed

--- AI/generate_embeddings.py
+++ AI/generate_embeddings.py
@@ -1,9 +1,24 @@
-from langchain_community.embeddings import OllamaEmbeddings
+from FlagEmbedding import BGEM3FlagModel
+
+model = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True)
 
 
-# Instantiation of the Ollama Embeddings model
+class BGEM3Embedding:
+    def embed_documents(self, texts):
+        return model.encode(
+            texts,
+            batch_size=4,
+            max_length=512,
+            return_dense=True
+        )["dense_vecs"].tolist()
+
+    def embed_query(self, text):
+        return model.encode(
+            [text],
+            max_length=512,
+            return_dense=True
+        )["dense_vecs"][0].tolist()
+
+
 def get_embeddings():
-    embeddings = OllamaEmbeddings(
-    model="nomic-embed-text",
-    )
-    return embeddings
+    return BGEM3Embedding()
